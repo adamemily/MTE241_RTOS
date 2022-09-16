53bcf5fa14d2de9246b7c0f0578e3f619a1b5067
adamemily <Emily.Canada2@gmail.com>
2022-09-16 17:28:52 -0400

    Lab1 Pt1, current PSP and MSP diff = 218

---
+++ src/_threadsCore.h
@@ -0,0 +1,15 @@
+#ifndef  __THREADSCORE_H
+
+#include <stdint.h>
+#include <stdio.h>
+#include <LPC17xx.h>
+
+//uint32_t max_stack = 0x2000;
+
+uint32_t* getMSPInitialLocation (void); //Obtains the initial location of MSP by looking it up in the vector table
+
+uint32_t* getNewThreadStack (uint32_t offset); //Returns address of new a PSP with offset of "offset" bytes from MSP.
+
+void setThreadingWithPSP (uint32_t* threadStack); // Sets the value of PSP to threadStack and ensures that the microcontroller is using that value by changing the CONTROL register
+
+#endif
